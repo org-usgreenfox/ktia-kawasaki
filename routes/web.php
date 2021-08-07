d03711935c5668495166c9a6af6c617cea63d64f
nmdz8eyc7ab <nmdz8eyc7ab@gmail.com>
2021-08-07 12:52:11 +0900

    created search page for store name

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::post('post/search', [PostController::class, 'search'])->name('post.search');
+
 Route::resource('post', PostController::class);
 
 Route::get('tag',[TagController::class, 'tagIndex'])->name('tag.index');
